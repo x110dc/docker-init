435df6326c08a93b728e84af4ede5e992fbad425
Daniel Craigmile <dcraigmile@texastribune.org>
2014-07-08 17:56:54 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+### Why use this?
+
+You're tired of typing some boilerplate for every docker-based project.  
+
+### What does it do?
+
+creates a `Dockerfile`, `Makefile`, and `.dockerignore` files so you don't have to
+start them from scratch.  The meat of it is the `Makefile` which I found myself
+be copying from project to project.
+
+### Installation:
+
+mkdir -p ~/bin
+export PATH=~/bin:$PATH
+
+### Usage:
+```
+> mkdir my-new-docker-project; cd my-new-docker-project
+> docker-init
+> ls -a
+Dockerfile
+Makefile
+.dockerignore
+```
+### Caveats:
+- this makes no attempt to check for existing files and will happily overwrite
+whatever is there
+
+### Prerequisites:
+- bash
---
+++ docker-init.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+(
+cat <<'EOF'
+APP=appname
+NAMESPACE=namespace
+
+build:
+        docker build --tag=${NAMESPACE}/${APP} .
+#
+#  You could use a completely different image to debug; maybe one with your
+#  favorite tools already present:
+#
+debug:
+        docker run --volumes-from=${APP} --interactive=true --tty=true ${NAMESPACE}/${APP} bash
+#
+#  The publish here will depend on your environment
+#
+run:
+        docker run --name=${APP} --detach=true --publish=80:8000 ${NAMESPACE}/${APP}
+clean:
+        docker stop ${APP} && docker rm ${APP}
+
+EOF
+) > Makefile
+
+(
+cat <<'EOF'
+#  decent chance you will not need .git as part of the context; for a big repo
+#  this will speed things up:
+.git
+EOF
+) > .dockerignore
+
+(
+cat <<'EOF'
+FROM ubuntu:trusty
+MAINTAINER foo@bar.org
+EOF
+) > Dockerfile
