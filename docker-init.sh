edef2db377c3cf9ae0cf758dde789a89589c4667
Daniel Craigmile <dcraigmile@texastribune.org>
2014-07-17 16:02:37 -0500

    add interactive to Makefile

--- docker-init.sh
+++ docker-init.sh
@@ -24,6 +24,8 @@ run:
 	docker run --name=${APP} --detach=true --publish=80:8000 ${NAMESPACE}/${APP}
 clean:
 	docker stop ${APP} && docker rm ${APP}
+interactive:
+	docker run --rm --interactive --tty --name=${APP} ${NAMESPACE}/${APP} bash
 
 EOF
 ) > Makefile
